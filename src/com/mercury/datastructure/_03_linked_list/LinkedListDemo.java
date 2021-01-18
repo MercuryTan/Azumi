61b1facea9fb05e8aeb5828603f4cf89dc300659
mercury <mercuryTan@163.com>
2021-01-18 12:41:31 +0800

    单向链表题目

--- src/com/mercury/datastructure/_03_linked_list/LinkedListDemo.java
+++ src/com/mercury/datastructure/_03_linked_list/LinkedListDemo.java
@@ -1,5 +1,7 @@
 package com.mercury.datastructure._03_linked_list;
 
+import java.util.Stack;
+
 /**
  *
  */
@@ -30,6 +32,56 @@ public class LinkedListDemo {
         list2.delete(n5);
         list2.print();
 
+        testQuesTion();
+
+
+    }
+
+    /**
+     * 测试题目
+     */
+    private static void testQuesTion() {
+        HeroNode n1 = new HeroNode(10, "Kobe");
+        HeroNode n2 = new HeroNode(20, "James");
+        HeroNode n3 = new HeroNode(30, "Curry");
+        LinkedList list = new LinkedList();
+        list.add(n1);
+        list.add(n2);
+        list.add(n3);
+        list.print();
+        //1、长度
+        System.out.println("===========长度================");
+        System.out.println(list.size());
+        //2、倒数k个节点值
+        System.out.println("===========倒数k值================");
+        System.out.println(list.getNode(3));
+        //3、反转
+        System.out.println("===========反转================");
+        list.reverse();
+        list.print();
+        //4、倒序打印
+        System.out.println("===========倒序打印================");
+        list.reversePrint();
+        //5、合并两链表
+        System.out.println("===========合并链表================");
+        HeroNode n4 = new HeroNode(11, "Kobe1");
+        HeroNode n5 = new HeroNode(21, "James1");
+        HeroNode n6 = new HeroNode(31, "Curry1");
+        LinkedList list2 = new LinkedList();
+        list2.addByOrder(n4);
+        list2.addByOrder(n5);
+        list2.addByOrder(n6);
+
+        HeroNode n7 = new HeroNode(12, "Kobe2");
+        HeroNode n8 = new HeroNode(22, "James2");
+        HeroNode n9 = new HeroNode(32, "Curry2");
+        LinkedList list3 = new LinkedList();
+        list3.addByOrder(n7);
+        list3.addByOrder(n8);
+        list3.addByOrder(n9);
+        LinkedList linkedList = LinkedList.mergeList(list2, list3);
+        linkedList.print();
+
     }
 }
 
@@ -38,7 +90,130 @@ public class LinkedListDemo {
  */
 class LinkedList {
     // 定义head节点，不存储具体数据
-    HeroNode head = new HeroNode(0, "");
+    private HeroNode head = new HeroNode(0, "");
+
+    //=====题目开始=====
+
+    /**
+     * 1、求单链表中节点的个数
+     */
+    public int size() {
+        if (head.next == null) {
+            return 0;
+        }
+        int length = 0;
+        HeroNode temp = head.next;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    /**
+     * 2、查找单链表中倒数第k个节点
+     *
+     * @param k
+     */
+    public HeroNode getNode(int k) {
+        int size = size();
+        if (size == 0) {
+            System.out.println("链表为空");
+            return null;
+        }
+        int curIndex = size - k;
+        if (curIndex < 0) {
+            System.out.println("链表长度不够");
+            return null;
+        }
+        int tmpLength = 0;
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while (temp != null) {
+            if (tmpLength == curIndex) {
+                flag = true;
+                break;
+            }
+
+            tmpLength++;
+            temp = temp.next;
+        }
+
+        if (flag) {
+            return temp;
+        }
+        return null;
+    }
+
+    /**
+     * 3、单链表的反转
+     * 方式：采用临时头节点保存
+     */
+    public void reverse() {
+        // 用于遍历
+        HeroNode temp = head.next;
+        HeroNode tHead = new HeroNode(0, "");
+        while (temp != null) {
+            HeroNode currentNode = temp;
+            //循环原链表
+            temp = temp.next;
+
+            currentNode.next = tHead.next;
+            tHead.next = currentNode;
+
+
+        }
+        head.next = tHead.next;
+    }
+
+
+    /**
+     * 4、从尾到头打印单链表
+     * 【反转：破坏原结构】
+     * 【栈】
+     */
+    public void reversePrint() {
+        Stack<HeroNode> stacks = new Stack<>();
+        HeroNode temp = head.next;
+        while (temp != null) {
+            stacks.push(temp);
+            temp = temp.next;
+        }
+
+        stacks.forEach(node -> {
+            System.out.println(node);
+        });
+    }
+
+
+    /**
+     * 5、合并两个有序的单链表，合并后的链表依然有序
+     *
+     * @param listOne 单链表one
+     * @param listTwo 单链表two
+     */
+    public static LinkedList mergeList(LinkedList listOne, LinkedList listTwo) {
+        // 遍历listTwo，并顺序插入到listOne中
+        HeroNode temp = listTwo.getHead().next;
+        while (temp != null) {
+            HeroNode currentNode = temp;
+            temp = temp.next;
+            listOne.addByOrder(currentNode);
+
+        }
+        return listOne;
+    }
+
+    //=====题目结束=====
+
+    /**
+     * 获取头结点
+     *
+     * @return
+     */
+    public HeroNode getHead() {
+        return this.head;
+    }
 
     public void delete(HeroNode node) {
         if (head.next == null) {
@@ -169,6 +344,7 @@ class LinkedList {
             }
             temp = temp.next;
         }
+        System.out.println("==========");
     }
 }
 
