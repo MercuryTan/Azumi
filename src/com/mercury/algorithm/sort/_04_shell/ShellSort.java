0c90ca897daad5fb0767142a21ef596cc1311c2e
mercury <mercuryTan@163.com>
2021-01-28 16:41:10 +0800

    排序：希尔排序(移位法)

--- src/com/mercury/algorithm/sort/_04_shell/ShellSort.java
+++ src/com/mercury/algorithm/sort/_04_shell/ShellSort.java
@@ -6,13 +6,13 @@ import java.util.Random;
 public class ShellSort {
 
     public static void main(String[] args) {
-//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
         ShellSort sort = new ShellSort();
 //        sort.shellSort(arr);
 //        sort.trueShellSort(arr);
 
-
-        int[] arr = new int[80000];
+        // 交换法
+        /*int[] arr = new int[80000];
         Random random = new Random();
         for (int i = 0; i < 80000; i++) {
             arr[i] = random.nextInt();
@@ -24,9 +24,74 @@ public class ShellSort {
         long betweenMs = end - begin;
         long second = betweenMs / 1000;
         System.out.println("begin:" + begin + "\t end:" + end + "\t 耗费时间：" + betweenMs + "ms;" + second + "秒");
+        System.out.println(Arrays.toString(arr));*/
+
+
+       /* int[] arr = new int[80000];
+        Random random = new Random();
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = random.nextInt();
+        }
+        long begin = System.currentTimeMillis();
+        sort.moveShellSort(arr);
+        long end = System.currentTimeMillis();
+
+        long betweenMs = end - begin;
+        long second = betweenMs / 1000;
+        System.out.println("begin:" + begin + "\t end:" + end + "\t 耗费时间：" + betweenMs + "ms;" + second + "秒");
+        System.out.println(Arrays.toString(arr));*/
+
+        sort.moveWhileShellSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     * 移位法： 只对有序列表的元素进行移位，等最后比较完，再将待插入元素插到对应的位置
+     */
+    public void moveShellSort(int[] arr) {
+        int j; // 待插入元素的前一位置
+        int insertVal; // 待插入元素
+        for (int inc = arr.length / 2; inc >= 1; inc /= 2) {
+            for (int i = inc; i < arr.length; i++) {
+                insertVal = arr[i];
+                for (j = i - inc; j >= 0 && insertVal < arr[j]; j -= inc) {
+                    // 将当前j位置往后移
+                    arr[j + inc] = arr[j];
+                }
+                // for循环中，最后是j-inc了，所以原j位置为j+inc  【for循环可以用while替换】
+                arr[j + inc] = insertVal;
+
+            }
+        }
+    }
+
+    /**
+     * 移位：使用while循环实现
+     * @param arr
+     */
+    public void moveWhileShellSort(int[] arr) {
+        int j; // 待插入元素的前一位置
+        int insertVal; // 待插入元素
+        for (int inc = arr.length / 2; inc >= 1; inc /= 2) {
+            for (int i = inc; i < arr.length; i++) {
+                insertVal = arr[i];
+                j = i - inc;
+                while (j >= 0 && insertVal < arr[j]) {
+                    arr[j + inc] = arr[j];
+                    j -= inc;
+                }
+                arr[j + inc] = insertVal;
+
+            }
+        }
+    }
+
+
+    /**
+     * 交换法: 两位进行交换
+     *
+     * @param arr
+     */
     public void trueShellSort(int[] arr) {
         for (int inc = arr.length / 2; inc >= 1; inc /= 2) {
             // 每一次分组(为什么要等于inc，因为插入排序中，是把前面的元素当成有序列表，是从无序列表中选择第一个元素进行插入)
